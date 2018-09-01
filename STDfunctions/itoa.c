54a9c66d1b04ca47d712f2396812d2dd75739d4b
esgzk <35306184+esgzk@users.noreply.github.com>
2018-09-01 17:04:04 +0300

    itoa.c

---
+++ STDfunctions/itoa.c
@@ -0,0 +1,17 @@
+char * itoa(int, int);
+
+
+char * itoa(int value, int base)
+{
+	
+	
+	static char buffer[32] = {0};
+	
+	int i = 30;
+	
+	for(; value && i ; --i, value /= base)
+	
+		buffer[i] = "0123456789abcdef"[value % base];
+	
+	return &buffer[i+1];
+}
